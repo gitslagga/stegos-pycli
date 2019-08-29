0ad4cdd9791d45d1fb8af65470e75460fa77cdfb
slagga <957766762@qq.com>
2019-08-29 17:25:53 +0800

    fix to addcount

--- setting.py
+++ setting.py
@@ -11,7 +11,7 @@ ACCOUNT_PKY     = '7fcHygDCR8NFQ8WxYnmXnJHbko65VWAPNH5kFs3gk6gNa2pRYB2'
 NETWORK_PKEY    = 'd91131dab0f9d7a6b78638883191819b96f231891f8eb8b2b4b8e69f6969ba04dcbf45ce349507fc50d339c646a6b30b1a92ca9f80a18b0f15cc54a4492ac9f47e96d41bcbc695b7d23b95905fa169e7f0f4710cd0fdf025cf76550cea3700b0'
 RECOVERY        = 'toss egg artwork elephant proof gentle common toss brush protect project boat toward usual sunny mountain ability lounge adult truly stumble apart lens budget'
 
-TO_ACCOUNT      = '7fd3kYCiBWSGg5bn7dC5EMWHLS8Ndsw8C2VFsHnzDcKmWmQ8jj6'
+TO_ACCOUNT      = '7fUHX92uHLtMtGKBgJ43sXj3rLAKVaqC2rA1ThARs6o3PoM3Wrv'
 TO_AMOUNT       = 1
 
 DINGDING_URL    = 'https://oapi.dingtalk.com/robot/send?access_token=de1ff8be041703322fd78444ceccb6c0fbb6a2d17ffbb0dbbb4d9c8ad58e7116'
